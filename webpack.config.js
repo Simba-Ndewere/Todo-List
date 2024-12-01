c9d0c916075c9ab4474560a61c37a7df7fc07b02
Simba Ndewere <ndeweresimba@gmail.com>
2024-12-01 04:37:50 +0000

    fix flash of unstyled conten

--- webpack.config.js
+++ webpack.config.js
@@ -3,27 +3,27 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
   },
-  devtool: "eval-source-map",
+  devtool: "inline-source-map",
   devServer: {
     watchFiles: ["./src/template.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+      filename: 'index.html',
+      inject: 'head',
     }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+            use: ['style-loader', 'css-loader'],
       },
     ],
   },
