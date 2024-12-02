1c04b53fa450cd9ae64854244fdf06f91a04b69c
Simba Ndewere <ndeweresimba@gmail.com>
2024-12-02 23:28:24 +0000

    design todo card showing check box, title, date and priority colour

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import "./index.css";
 import Todo from "./todo";
+import createTodo from "./todoDom";
+
 //create todos, 
 //edit todos,
 //change dom
 //save to local storage
+//view todo's
 
 const todo1 = new Todo("odin project", "entire course", "12/23/2024", "Medium", false);
 console.log(todo1);
+createTodo(todo1);
+createTodo(todo1);
